cfa45bb4c8a5681a3e5008a81684629c1d114a06
Zac Cohan <zac@acqualia.com>
2020-02-18 02:54:03 +0300

    Initial commit
    With a simple Mac and iOS SoulverCore example

---
+++ MacSample/MacSample/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MacSample
+//
+//  Created by Zac Cohan on 8/2/20.
+//  Copyright © 2020 Zac Cohan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
