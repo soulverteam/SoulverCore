0e1b43fc99821706548cdf71206657c48ca63db5
Zac Cohan <zac@acqualia.com>
2020-09-26 13:38:54 +0300

    Update checksum for Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
         .binaryTarget(
             name: "SoulverCore",
             url: "https://github.com/soulverteam/SoulverCore/releases/download/1.1.2/SoulverCore.xcframework.zip",
-            checksum: "9eb58b5fd1eda5157734dee414a8715f695dfac2e1a2c29364c5a6fecc118675"),
+            checksum: "7eeeff4e925faf039fd22eb5d208dd2aac344821a910483ebe3f1921ebd46953"),
     ]
 )
