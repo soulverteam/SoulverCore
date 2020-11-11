d5a5b5ce16b03451a22736c9db86b000a92cb0db
Zac Cohan <zac@acqualia.com>
2020-11-11 08:54:45 +0300

    Update SoulverCore framework (v1.1.5)
    This version adds native Apple Silicon support

---
+++ SoulverCore.xcframework/ios-arm64_x86_64-maccatalyst/SoulverCore.framework/Versions/A/Headers/SoulverCore.h
@@ -0,0 +1,17 @@
+//
+//  SoulverCore.h
+//  SoulverCore
+//
+//  Created by Zac Cohan on 3/10/18.
+//  Copyright © 2020 Zac Cohan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+// In this header, you should import all the public headers of your framework using statements like #import <SoulverCore/PublicHeader.h>
+
+//! Project version number for SoulverCore.
+FOUNDATION_EXPORT double SoulverCoreVersionNumber;
+
+//! Project version string for SoulverCore.
+FOUNDATION_EXPORT const unsigned char SoulverCoreVersionString[];
+
