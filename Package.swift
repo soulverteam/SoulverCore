ae908a2510203b311f7b9245982c0e6555b9d890
Zac Cohan <zac@acqualia.com>
2022-11-08 19:20:05 +0300

    Update SoulverCore framework (v2.2.6)
    Compiled with Xcode 14.1 and Swift 5.7.1

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SoulverCore",
-            url: "https://github.com/soulverteam/SoulverCore/releases/download/2.2.5/SoulverCore.xcframework.zip",
-            checksum: "0eddd4a3c6af43ae2f16cd8ff44acfe431a5053e3197a35372ccce2206b16ef2"),
+            url: "https://github.com/soulverteam/SoulverCore/releases/download/2.2.6/SoulverCore.xcframework.zip",
+            checksum: "15a0f1833db9d0c4ab48279e9364cdf0b67d65245f6d4afac7f8e436a3be53fb"),
     ]
 )
