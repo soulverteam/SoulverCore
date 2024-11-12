895c5b1427659b863981015509b65cc1c6fe84a9
Zac Cohan <zac@acqualia.com>
2024-11-12 16:32:58 +1100

    Add an example of a PlaceDataProvider
    With implementations of placeDataFor(request: String) and distanceBetween(location1: SoulverCore.PlaceData.Coordinates, location2: SoulverCore.PlaceData.Coordinates) -> SoulverCore.UnitExpression? that utilize CoreLocation.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SoulverCore",
-            url: "https://github.com/soulverteam/SoulverCore/releases/download/2.7.1/SoulverCore.xcframework.zip",
-            checksum: "c875356ccafc515d007ae89ab9947a03e6774080941b61a98a7084c5a141f079"),
+            url: "https://github.com/soulverteam/SoulverCore/releases/download/3.0.0/SoulverCore.xcframework.zip",
+            checksum: "899988bbf0444dd4db303f5bbc88f2f6b5a0ca58ab4e878b7ecabcef5aa8ab43"),
     ]
 )
