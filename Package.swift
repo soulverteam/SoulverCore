a38a1d0346a4d2bb3c78ae1812b07eb941e126c0
Zac Cohan <zac@acqualia.com>
2024-11-22 14:26:11 +1100

    Update framework to release build of 3.0.0

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
         .binaryTarget(
             name: "SoulverCore",
             url: "https://github.com/soulverteam/SoulverCore/releases/download/3.0.0/SoulverCore.xcframework.zip",
-            checksum: "899988bbf0444dd4db303f5bbc88f2f6b5a0ca58ab4e878b7ecabcef5aa8ab43"),
+            checksum: "2d5375c1dc438d0a8ab54a73c956cdf2fa7af1bc11d36501f56dff3f7168bccb"),
     ]
 )
